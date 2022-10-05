92d87f147079ea21d7c728260831230f38c99600
Mohit Gupta <mohgupt@adobe.com>
2022-10-06 01:03:58 +0530

    Day 2 Started

---
+++ StriverSheet/Day2/MergeOverlappingIntervals.java
@@ -0,0 +1,82 @@
+package StriverSheet.Day2;
+
+import java.util.*;
+
+// LC 56: https://leetcode.com/problems/merge-intervals/
+
+// Approach:
+// 1. Sort the elements by Start time - For this create class Pair with Comparable's compareTo() function
+// and convert the pairs into array of Pair class. 
+// Use Arrays.sort() and pass Pair[] array to it. Note that Arrays.sort() works only on arrays. 
+
+// 2. After Sorting, run a loop to create Pair objects and push elements into a Stack. Elements are 
+// created such that if array's current element's End time is greater than Stack's top element's Start time than
+// update the End time of stack's element, else, push the new Pair element to Stack.
+
+//3. Pop the Stack's elements and create a new array[][] (as original input) to return as answer.
+
+class Pair implements Comparable<Pair> {
+    int start;
+    int end;
+
+    Pair(int st, int end) {
+        this.start = st;
+        this.end = end;
+    }
+
+    public int compareTo(Pair other) {
+        return this.start - other.start;
+    }
+}
+
+public class MergeOverlappingIntervals {
+
+    public static int[][] merge(int[][] intervals) {
+        Pair[] pairs = new Pair[intervals.length];
+
+        for (int i = 0; i < pairs.length; i++) {
+            pairs[i] = new Pair(intervals[i][0], intervals[i][1]);
+        }
+        Arrays.sort(pairs);
+        Stack<Pair> st = new Stack<>();
+
+        for (int i = 0; i < pairs.length; i++) {
+            // push the first object to Stack
+            if (i == 0) {
+                st.push(pairs[0]);
+            } else {
+                Pair p = st.peek();
+                if (pairs[i].start < p.end) {
+                    p.end = Math.max(p.end, pairs[i].end);
+                } else {
+                    st.push(pairs[i]);
+                }
+            }
+        }
+
+        int[][] result = new int[st.size()][intervals[0].length];
+        
+        for (int j = 0; j < result.length; j++) {
+            Pair p = st.pop();
+            result[j][0] = p.start;
+            result[j][1] = p.end;
+        }       
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int intervals[][] = { { 1, 3 }, { 8, 10 }, { 2, 6 }, { 15, 18 } };
+
+        int result[][] = merge(intervals);
+
+        //printing result
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
---
+++ StriverSheet/Day2/RotateMatrix.java
@@ -0,0 +1,73 @@
+package StriverSheet.Day2;
+
+// LC 48: https://leetcode.com/problems/rotate-image/
+// Rotate matrix by 90 degree in-place
+
+public class RotateMatrix {
+
+    // Bruteforce approach (Not in-place):
+    // Create another matrix and place first row into last column , second row and
+    // second last column..etc..
+    public static int[][] RotateMatrix_Bruteforce(int[][] matrix) {
+        int[][] rotatedMatrix = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < rotatedMatrix.length; i++) {
+            for (int j = 0; j < rotatedMatrix[0].length; j++) {
+                rotatedMatrix[j][rotatedMatrix.length - i - 1] = matrix[i][j];
+            }
+        }
+        return rotatedMatrix;
+    }
+
+    // Optimised approach:
+
+    // Rotating a matrix is equivalent to :
+    // Step 1: Transpose the matrix
+    // Step 2: Reverse each row
+
+    public static int[][] RotateMatrix_Optimised(int[][] matrix) {
+
+        // Transposing a matrix
+        // Notice to transpose:  'j' has to start from i and not 0
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) { 
+                swap(matrix, i, j, i, j);
+            }
+        }
+
+        // Reversing each row
+        // Notice to rotate:  we are moving 'j' only till half
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length / 2; j++) { 
+                swap(matrix, i, j, matrix[0].length - 1 - j, i);
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void swap(int[][] matrix, int i1, int j1, int i2, int j2) {
+        int temp = matrix[i1][j1];
+        matrix[i1][j1] = matrix[j2][i2];
+        matrix[j2][i2] = temp;
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        printMatrix(arr);
+        int rotated[][] = RotateMatrix_Optimised(arr);
+        System.out.println("Rotated Image");
+        printMatrix(rotated);
+
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
